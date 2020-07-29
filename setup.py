4fd1fda91e771c9838401915d6b5850fefbca504
joshuadrc <joshuadrc@gmail.com>
2020-07-29 16:31:16 -0400

    adding final

--- setup.py
+++ setup.py
@@ -4,23 +4,24 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="food2vec", # Replace with your own username
-    version="0.0.2",
+    name="food2vec",
+    version="0.0.3",
     author="Joshua D'Arcy",
     author_email="joshuadrc@gmail.com",
     description="Tools for semantic nutrition",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
+    packages = ["food2vec"],
     install_requires =[
-        'numpy',
-        'pandas',
-        'requests',
+        "numpy",
+        "pandas",
+        "requests",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3',
+    python_requires=">=3",
 )
